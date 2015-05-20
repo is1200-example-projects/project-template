489968ab11c3dd93d86735d2d2a50384c8668c5f
h4xxel <isaksson.axel@telia.com>
2015-05-20 14:19:47 +0200

    commented stubs.c

--- stubs.c
+++ stubs.c
@@ -1,12 +1,14 @@
-
+/* Non-Maskable Interrupt; something bad likely happened, so hang */
 void _nmi_handler() {
 	for(;;);
 }
 
-void _on_bootstrap() {
+/* This function is called upon reset, before .data and .bss is set up */
+void _on_reset() {
 	
 }
 
-void _on_reset() {
+/* This function is called before main() is called, you can do setup here */
+void _on_bootstrap() {
 	
 }
